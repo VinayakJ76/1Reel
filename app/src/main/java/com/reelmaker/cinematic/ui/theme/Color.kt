7b45a70f1567fb84a01d1b75f9845629cfb44e28
Shakthi Vinayak <138783432+VinayakJ76@users.noreply.github.com>
2026-04-03 18:54:18 +0530

    Remove inline camera permission check from Compose click path

---
+++ app/src/main/java/com/reelmaker/cinematic/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.reelmaker.cinematic.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Midnight = Color(0xFF0F1022)
+val Violet = Color(0xFF8E7CFF)
+val Magenta = Color(0xFFE879F9)
+val Mint = Color(0xFF2DE2E6)
+val Slate = Color(0xFF27293D)
